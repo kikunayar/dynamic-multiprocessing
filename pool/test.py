c8d2dda4668b850d120389acb31f185cbf14242f
kikunayar <huntergeen@hotmail.com>
2024-08-29 12:43:40 +0700

    Update test.py

--- pool/test.py
+++ pool/test.py
@@ -14,4 +14,4 @@ def start(shared_dict):
 if __name__=='__main__':
     import os
     from engine._mod_ import update
-    update(os.path.basename(__file__),'upload')
+    update(os.path.basename(__file__),'replace')
